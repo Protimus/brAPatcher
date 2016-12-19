430ece3e1875df7cff6e3995496cb17b3add4691
Rafael Gorges <rafaelgorges@outlook.com>
2016-12-19 16:13:26 -0200

    - QFile verification of a exist style file. - Progress Bar style.

--- app/brapatcher.cpp
+++ app/brapatcher.cpp
@@ -1,11 +1,24 @@
 #include "interface.h"
 #include <QApplication>
+#include <QFile>
+#include <QTextStream>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    QFile f(":style/ancient.qss");
+    if (!f.exists())
+    {
+        printf("Unable to set stylesheet, file not found\n");
+    }
+    else
+    {
+        f.open(QFile::ReadOnly | QFile::Text);
+        QTextStream ts(&f);
+        qApp->setStyleSheet(ts.readAll());
+    }
 
     return a.exec();
 }
